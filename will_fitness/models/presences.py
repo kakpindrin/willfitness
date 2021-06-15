a2de1e10c9a9ac0436fd529be95562a6d7957b40
Kavola <kavolad@gmail.com>
2021-06-15 18:06:44 +0000

    Thank you so much Lord.

--- will_fitness/models/presences.py
+++ will_fitness/models/presences.py
@@ -11,12 +11,18 @@ class will_fitness_presence(models.Model):
     client = fields.Many2one('res.partner', string="Client",)
     arrivee = fields.Datetime(string='Arrivée', default=fields.Datetime.now,)
     sortie = fields.Datetime(string='Sortie', default=fields.Datetime.now,)
-    duree_de_travail = fields.Float(string='Durée de travail',)
+    duree_de_travail = fields.Float(string='Durée de travail', readonly=True)
 
     @api.model
     def create(self, vals):
         if vals.get('name', _('New')) == _('New'):
             vals['name'] = self.env['ir.sequence'].next_by_code('will.fitness.presence.sequence',) or _('New')
         result = super(will_fitness_presence, self).create(vals)
-    
+
+        #a = datetime.now().time()
+        time_one = result.sortie.hour + result.sortie.minute/60.0
+        time_two = result.arrivee.hour + result.arrivee.minute/60.0
+        result.write({
+            'duree_de_travail': time_two - time_one
+        })
         return result
