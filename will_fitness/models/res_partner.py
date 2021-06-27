e0d1c2af7f3ae861d6dbaa31c54927fc28f7b98e
Kavola <kavolad@gmail.com>
2021-06-27 21:44:22 +0000

    Thank you Lord so much.

--- will_fitness/models/res_partner.py
+++ will_fitness/models/res_partner.py
@@ -22,7 +22,7 @@ class res_partner(models.Model):
         ('barcode_uniq', 'unique (barcode)', "The Badge ID must be unique, this one is already assigned to another partner."),
     ]
 
-    #THINK
+    #THINK GOOD
     def generate_random_barcode(self):
         for partner in self:
             partner.barcode = '041'+"".join(choice(digits) for i in range(9))
