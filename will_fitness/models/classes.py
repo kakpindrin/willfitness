a7928b91db77de9ad8bfeacb9de74015a23d3ea0
Kavola <kavolad@gmail.com>
2021-06-30 12:12:34 +0000

    Merci Seigneur Éternel Notre DIEU.

---
+++ will_fitness/models/classes.py
@@ -0,0 +1,16 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+from datetime import timedelta, datetime, date
+#from xl2dict import XlToDict
+
+
+class will_fitness_classe(models.Model):
+    _name = 'will.fitness.classe'
+    _description = "Classe des eleves"
+
+    #infos à saisir pour la classe
+    name = fields.Char(string="Nom de la Classe", copy=False, index=True, required=True,)
+    
+    @api.model
+    def create(self, vals):
+        result = super(will_fitness_classe, self).create(vals)
